6e8d5788e1831e4cdb695e4596db94289064bb6a
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-07-24 20:40:47 +0100

    Complete checkArguments

--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -2,8 +2,11 @@
 import { checkArguments } from "./assert-equals";
 
 describe("checkArguments function test", () => {
-  test("Missing argument", () => {
+  test("One missing argument", () => {
     expect(checkArguments(1)).toBe(false);
+  });
+  test("No arguments at all", () => {
+    expect(checkArguments()).toBe(false);
   });
   test("Working with both arguments", () => {
     expect(checkArguments("hello OakNorth", 666)).toBe(true);
